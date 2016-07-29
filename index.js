157a4b6eb553e9a1775703fa47650ffa8f921600
Andrew Laiff <q.laiff@gmail.com>
2016-07-29 14:44:46 +0500

    Plugin as peer

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var modify = require('modify-babel-preset');
 
 module.exports = modify('es2015-loose', {
-    'transform-es2015-modules': false,
-    'transform-es2015-modules-optimized': {
+    'transform-es2015-modules-commonjs': false,
+    'transform-es2015-modules-commonjs-optimized': {
         loose: true
     }
 });
