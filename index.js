7492bc2d726d5693c529052c9da3fa85a910fce0
Andrew Laiff <q.laiff@gmail.com>
2016-07-29 14:36:18 +0500

    Switch to base loose preset

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var modify = require('modify-babel-preset');
 
-module.exports = modify('es2015', {
+module.exports = modify('es2015-loose', {
     'transform-es2015-modules': false,
-    'transform-es2015-modules-optimized': true
+    'transform-es2015-modules-optimized': {
+        loose: true
+    }
 });
